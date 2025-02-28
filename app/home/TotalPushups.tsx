66e3153228f6ff2e91d4b2e734483a8b2f98141e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2025-03-01 00:20:33 +0300

    ✨ update TotalPushups component to display the current month dynamically

--- app/home/TotalPushups.tsx
+++ app/home/TotalPushups.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import dayjs from "dayjs";
 import { use } from "react";
 
+const months = new Map([
+  [1, "January"],
+  [2, "February"],
+  [3, "March"],
+  [4, "April"],
+  [5, "May"],
+  [6, "June"],
+  [7, "July"],
+  [8, "August"],
+  [9, "September"],
+  [10, "October"],
+  [11, "November"],
+  [12, "December"],
+]);
 const TotalPushups = ({
   totalCount,
 }: {
@@ -11,7 +26,9 @@ const TotalPushups = ({
 
   return (
     <div className="text-center w-full flex flex-col gap-4">
-      <h1 className="text-xl font-bold">Total Pushups</h1>
+      <h1 className="text-xl font-bold">
+        Total Pushups for {months.get(dayjs().month() + 1)}
+      </h1>
       <div className="text-5xl font-bold">{count}</div>
     </div>
   );
